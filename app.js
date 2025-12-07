026506078e8367b490d0767ffd9cdb407bcbe5a3
orokidd <gregomily@gmail.com>
2025-12-07 18:04:13 +0800

    page error and server error handler

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 // import indexRouter from './routes/index.routes.js';
 
 import initializePassport from './config/passport.js';
+import errorHandler from './middleware/errorHandler.js'
 
 const app = express()
 
@@ -40,85 +41,12 @@ initializePassport(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use('/', indexRouter)
+app.use(errorHandler.notFound)
+app.use(errorHandler.serverError)
 
-// app.get("/dashboard", checkAuthentication, async (req, res) => {
-//     const user = req.user;
-//     const files = await prisma.file.findMany({
-//         where: { userId: user.id }
-//     });
-//     const folders = await prisma.folder.findMany({
-//         where: { userId : user.id }
-//     })
-//     res.render("dashboard", { user, files, folders }); 
-// });
-
-// app.get("/upload", checkAuthentication, (req, res) => {
-//   res.render("upload")
-// } )
-
-// app.post("/uploadFile", checkAuthentication, upload.single('file'), async (req, res) => {
-//   await prisma.file.create({
-//     data: {
-//       name: req.file.filename,
-//       size: req.file.size,
-//       url: req.file.path,
-//       userId: req.user.id
-//     }
-//   })
-//   res.redirect("/dashboard")
-// })
-
-// app.get("/folder/:id", checkAuthentication, async (req, res) => {
-//   const folderId = req.params.id
-//   const user = req.user;
-
-//   const folder = await prisma.folder.findUnique({
-//     where: { id: parseInt(folderId) , userId: user.id}
-//   })
-//   const files = await prisma.file.findMany({
-//       where: { folderId: parseInt(folderId), userId: user.id }
-//   });
-//   res.render("folder", { user, folder, files });
-// })
-
-// app.post("/folder", checkAuthentication, async (req, res) => {
-//   await prisma.folder.create({
-//     data: {
-//       name: req.body.foldername,
-//       userId: req.user.id
-//     }
-//   })
-//   res.redirect("/dashboard")
-// })
-
-// app.post("/folder/:id/uploadFile", checkAuthentication, upload.single('file'), async (req, res) => {
-//   const folderId = req.params.id
-  
-//   await prisma.file.create({
-//     data: {
-//       name: req.file.filename,
-//       size: req.file.size,
-//       url: req.file.path,
-//       userId: req.user.id,
-//       folderId: parseInt(folderId)
-//     }
-//   })
-// })
-
-// app.get("/file/:fileId", checkAuthentication, async (req, res) =>{
-//   const fileId = req.params.fileId
-//   const file = await prisma.file.findUnique({
-//     where: { id: parseInt(fileId) }
-//   })
-
-//   res.render("fileDetails", { user: req.user, file })
-// })
-
-// app.get("/file/:fileId/download", checkAuthentication, async (req, res) => {
-//   const fileId = req.params.fileId
-//   const file = await prisma.file.findUnique({
-//     where: { id: parseInt(fileId) }
-//   })
-//   res.download(file.url, file.name)
-// })
+app.listen(3000, (error) => {
+  if (error) {
+    throw error
+  }
+  console.log('Server is running on port 3000');
+});

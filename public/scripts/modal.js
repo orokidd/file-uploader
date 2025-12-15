d67a93a6368b9ba9f5c804ac916557fabb48e27a
orokidd <gregomily@gmail.com>
2025-12-15 08:30:57 +0800

    new folder uses modal instead of just form

---
+++ public/scripts/modal.js
@@ -0,0 +1,27 @@
+// const deleteModal = document.querySelector("#delete-modal")
+// const confirmButton = document.querySelector(".confirm-btn")
+// const cancelButton = document.querySelector(".cancel-btn")
+// const deleteForm = document.querySelector("#post-delete-form")
+
+// function openDeleteModal(actionLink) {
+//     deleteModal.style.display = "flex"
+//     deleteForm.action = actionLink
+
+//     cancelButton.addEventListener("click", () => {
+//         deleteModal.style.display = "none";
+//     });
+
+//     window.addEventListener("click", (e) => {
+//         if (e.target === deleteModal) deleteModal.style.display = "none";
+//     });
+// }
+
+const newFolderModal = document.querySelector(".new-folder-form-modal")
+
+function openNewFolderModal() {
+    newFolderModal.style.display = "flex"
+
+    window.addEventListener("click", (e) => {
+        if (e.target === newFolderModal) newFolderModal.style.display = "none";
+    });
+}

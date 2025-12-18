24f708982a3bfd3fb28dd85fb763a812807f5601
orokidd <gregomily@gmail.com>
2025-12-18 11:27:56 +0800

    delete folder routes

--- routes/folders.routes.js
+++ routes/folders.routes.js
@@ -5,6 +5,7 @@ const foldersController = require('../controllers/folders.controller.js');
 const router = express.Router();
 
 router.get("/folders/:folderId", checkAuthentication, foldersController.getFolder);
+router.post("/folders/:folderId/delete", checkAuthentication, foldersController.deleteFolder);
 router.post("/folders", checkAuthentication, foldersController.createFolder);
 
 module.exports = router;
